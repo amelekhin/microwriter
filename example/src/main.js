3e0f877396444f69c173fefed10c8ed1f91c1239
Anton Amelekhin <a.amelekhin@outlook.com>
2020-05-21 10:21:16 +0300

    Update example with the code from README

--- example/src/main.js
+++ example/src/main.js
@@ -1,15 +1,36 @@
 import Microwriter from '../../dist/microwriter';
 
+// Find a target element to write text into
 const target = document.getElementById('microwriter-target');
 
+// Initialize a writer
 const writer = new Microwriter({
+  // The target
   target,
+
+  // The lines list.
+  // They are printend in infinite loop.
   lines: ['Hello, world', 'This is Microwriter'],
+
+  // The writing speed in milliseconds.
   writeSpeed: 150,
+
+  // The deletion speed milliseconds.
+  // If not provided, writeSpeed used instead.
   deleteSpeed: 50,
+
+  // A delay in milliseconds before writing a new line
   writeLineDelay: 400,
+
+  // A delay in milliseconds before deletion a written line
   deleteLineDelay: 1000,
 });
 
+// Start writing
 writer.start();
-console.log(writer);
+
+// Pause writing
+// writer.pause();
+
+// Replace lines and restart
+// writer.replaceLines(['My next list', 'of lines.']);
