d51cb7bfb40e4d2ec78a40c6714ea25839637757
Anton Amelekhin <a.amelekhin@outlook.com>
2020-05-21 11:05:12 +0300

    Rebuild

--- dist/microwriter.d.ts
+++ dist/microwriter.d.ts
@@ -6,11 +6,11 @@ interface MicrowriterOptions {
     /** A speed in milliseconds between typing new characters */
     writeSpeed: number;
     /** A speed in milliseconds between deletion of already typed characters */
-    deleteSpeed: number;
+    deleteSpeed?: number;
     /** A delay between before typing the line */
-    writeLineDelay: number;
+    writeLineDelay?: number;
     /** A delay between before deleting the line */
-    deleteLineDelay: number;
+    deleteLineDelay?: number;
 }
 declare class Microwriter {
     /** An HTML element to write into */
